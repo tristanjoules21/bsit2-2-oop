0aea256bc98d2cbc067f7b48620b8acdbea7c9fa
Tristan Joules C. Pahayahay <tjpahayahay67260@liceo.edu.ph>
2025-08-04 11:16:40 +0800

    activity 2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    static int number = 0;
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int total = 0;
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter number " + i + ": ");
-            int number = scanner.nextInt();
-            total += number;
+        for (int i = 1; i <= 5; i++){
+            System.out.print("Enter Number " + i + ":");
+            number = number + scanner.nextInt();
         }
-
-        System.out.println("Total: " + total);
-        scanner.close();
+        System.out.println("Total Number: " + number);
     }
 }
