63079b2009db92b5a58a9cc06b03c293c04099ae
Tristan Joules C. Pahayahay <tjpahayahay67260@liceo.edu.ph>
2025-08-08 09:45:49 +0800

    prelim-exam

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,31 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Student s1 = new Student("Alice", 20, "BSIT", 85, 90, 88);
+        Student s2 = new Student("Bob", 21, "BSCS", 92, 95, 89);
+        Student s3 = new Student("Charlie", 19, "BSIT", 65, 70, 68);
+
+        Student[] list = {s1, s2, s3};
+
+        int passCount = 0;
+
+        for (Student s : list) {
+            s.showInfo();
+            double avg = s.getAverage();
+            String grade = s.getGrade();
+            boolean pass = s.isPass();
+
+            System.out.printf("Average: %.2f\n", avg);
+            System.out.println("Letter Grade: " + grade);
+            System.out.println("Status: " + (pass ? "PASSING" : "FAILING"));
+
+
+            if (pass) {
+                passCount++;
+            }
+        }
+
+        System.out.println("Summary: " + passCount + " out of 3 students are passing");
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,39 @@
+public class Student {
+    String name;
+    int age;
+    String course;
+    double g1, g2, g3;
+
+    public Student(String name, int age, String course, double g1, double g2, double g3) {
+        this.name = name;
+        this.age = age;
+        this.course = course;
+        this.g1 = g1;
+        this.g2 = g2;
+        this.g3 = g3;
+    }
+
+    public void showInfo() {
+        System.out.println("Name: " + name + ", Age: " + age + ", Course: " + course );
+
+        System.out.println("Grades :" + g1 +", "+ g2 +", " + g3);
+
+    }
+
+    public double getAverage() {
+        return (g1 + g2 + g3) / 3;
+    }
+
+    public String getGrade() {
+        double avg = getAverage();
+        if (avg >= 90) return "A";
+        else if (avg >= 80) return "B";
+        else if (avg >= 70) return "C";
+        else if (avg >= 60) return "D";
+        else return "F";
+    }
+
+    public boolean isPass() {
+        return getAverage() >= 70;
+    }
+}
