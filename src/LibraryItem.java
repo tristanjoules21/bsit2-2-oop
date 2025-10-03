cfe848d6c304fccc8b22ee9deffe4da73db4587f
tristanjoules21 <tjpahayahay67260@liceo.edu.ph>
2025-10-03 09:43:09 +0800

    Create LibraryItem.java

---
+++ src/LibraryItem.java
@@ -0,0 +1,32 @@
+public abstract class LibraryItem {
+    protected String itemId;
+    protected String title;
+    protected String author;
+    protected boolean isCheckedOut;
+    protected String borrowerName;
+
+    public LibraryItem(String itemId, String title, String author) {
+        this.itemId = itemId;
+        this.title = title;
+        this.author = author;
+        this.isCheckedOut = false;
+        this.borrowerName = null;
+    }
+
+    public String getItemInfo() {
+        return title + " by " + author + " (" + getItemType() + ")";
+    }
+
+    public void checkOut(String borrowerName) {
+        this.isCheckedOut = true;
+        this.borrowerName = borrowerName;
+    }
+
+    public void checkIn() {
+        this.isCheckedOut = false;
+        this.borrowerName = null;
+    }
+
+    public abstract String getItemType();
+    public abstract double calculateLateFee(int daysLate);
+}
